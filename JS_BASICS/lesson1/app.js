7e71c530c80b0e0d54c7b1aa2d052f013b112f48
Kaayzeer <mautiagiro@gmail.com>
2023-10-18 11:36:52 +0200

    JS lesson 2

---
+++ JS_BASICS/lesson1/app.js
@@ -0,0 +1,63 @@
+// PRIMITIVE DATATYPES
+// string
+var firstName = 'Kimbo';
+
+// number
+var age = 6.2;
+
+
+age = 5;
+
+
+// boolean
+var isGirl = false;
+var isBoy = true;
+// undefined
+var lastName;
+// null
+var zero = null;
+
+
+
+// NON-PRIMITIVE DATATYPES
+// object
+
+var car = {
+    color: 'red',
+    wheels: 4,
+    doors: 5,
+    model: 'A4',
+    brand: 'AUDI'
+};
+
+
+
+//arrays
+var people = ['ken', 'ryu', 'blanka', 'sagat'];
+
+people[4] = 'niko';
+
+
+//functions
+
+function logName(name) {
+console.log("hi my name is:", name);
+}
+
+logName("John Doe");
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log("sum(5, 5)", sum(5, 0));
+sum(5, );
+
+/* var people = [
+    ['ken', 'ryu', 'blanka', 'sagat'], 
+    ['scorpion', 'sub-zero'], 
+    ['mario', 'yoshi']
+] */
+
+
+
---
+++ JS_BASICS/lesson2/app.js
@@ -0,0 +1,32 @@
+
+/*
+ -----------------------
+ THE TODO OF THE DAY:
+ * Remove a TODO from the list
+ * Add a new TODO and append it in the todo list
+-----------------------
+ */
+
+// DOM manipulation
+
+// getElementById()
+
+// getElementByClassName()
+
+// getElementsByTagName()
+
+// querySelector()
+
+// QuerySelectorAll()
+
+//Dom events
+/* var list = document.querySelector('#todo-list');
+
+ */
+
+/* addEventListener('click', () => {
+
+}) */
+
+
+
